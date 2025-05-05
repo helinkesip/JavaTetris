b50c47d802d8a232e90eb347a04b6dd2882ac146
Rugeyye Çifci <rugeyyecifci05@gmail.com>
2025-05-05 15:02:03 +0300

    Update GameUtils.java

--- src/com/tetris/utils/GameUtils.java
+++ src/com/tetris/utils/GameUtils.java
@@ -1,12 +1,31 @@
 package com.tetris.utils;
 
+import com.tetris.model.Tetromino;
+import java.awt.Color;
+
 public class GameUtils {
-    /* yardımcı fonksiyonlar: Puan hesaplama, rastgele şekil üretme gibi ortak işlevler
+    private static final int[][][] TETROMINO_SHAPES = {
+            {{1, 1, 1, 1}}, // I
+            {{1, 1}, {1, 1}}, // O
+            {{0, 1, 0}, {1, 1, 1}}, // T
+            {{0, 0, 1}, {1, 1, 1}}, // L
+            {{1, 0, 0}, {1, 1, 1}}, // J
+            {{0, 1, 1}, {1, 1, 0}}, // S
+            {{1, 1, 0}, {0, 1, 1}}  // Z
+    };
 
-       Tekrar kullanılabilir kod: Farklı sınıflarda kullanılacak metodlar
+    private static final Color[] COLORS = {
+            new Color(0, 255, 255), // I - Cyan
+            new Color(255, 255, 0), // O - Yellow
+            new Color(170, 0, 255), // T - Purple
+            new Color(255, 165, 0), // L - Orange
+            new Color(0, 0, 255),   // J - Blue
+            new Color(0, 255, 0),   // S - Green
+            new Color(255, 0, 0)    // Z - Red
+    };
 
-       Düzenli yapı: Model/View/Controller dışındaki yardımcı kodları gruplamak*/
-
-
-
+    public static Tetromino createRandomTetromino() {
+        int randomIndex = (int)(Math.random() * TETROMINO_SHAPES.length);
+        return new Tetromino(TETROMINO_SHAPES[randomIndex], COLORS[randomIndex]);
+    }
 }
